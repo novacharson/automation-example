658d23a23f016722a9ec1e679dc9352ac85ba2c4
novacharson <30352062+novacharson@users.noreply.github.com>
2017-11-22 14:44:07 -0600

    simplified our solution xpath
    simplified our solution xpath

--- lib/hubbub/automation/welcome_page.rb
+++ lib/hubbub/automation/welcome_page.rb
@@ -1,6 +1,6 @@
 class WelcomePage < Capybara::PageMapper::Base
   define_button :why_hubbub, '//a[@title="Why hubbub"]'
-  define_button :our_solution, '//li[@class="nav-our-solution nav-current"]/a'
+  define_button :our_solution, '//li[@class="nav-our-solution nav-current"]'
 
   def our_solution!
     our_solution_button.trigger('click')
