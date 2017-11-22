fe5bb7b625db47ad52d8f56fcfb28af9a5c90c4f
novacharson <30352062+novacharson@users.noreply.github.com>
2017-11-22 14:51:01 -0600

    fixed menu link error for our solution
    changed xpath to a valid one.

--- lib/hubbub/automation/welcome_page.rb
+++ lib/hubbub/automation/welcome_page.rb
@@ -1,6 +1,6 @@
 class WelcomePage < Capybara::PageMapper::Base
   define_button :why_hubbub, '//a[@title="Why hubbub"]'
-  define_button :our_solution, '//li[@class="nav-our-solution nav-current"]'
+  define_button :our_solution, '//nav[@class="top"]//a[@href="/cms/our-solution/"]'
 
   def our_solution!
     our_solution_button.trigger('click')
